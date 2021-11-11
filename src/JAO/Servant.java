c4abe704d1a111413901142a794daea05528b12a
Karol Szustakowski <szustarol123@gmail.com>
2021-11-11 12:35:04 +0100

    Servant implementation

--- src/JAO/Servant.java
+++ src/JAO/Servant.java
@@ -5,21 +5,39 @@ import java.util.ArrayList;
 public class Servant<T> {
     private T [] buffer;
 
-    private long writeIndex = 0;
-    private long readIndex = 0;
+    private int writeIndex = 0;
+    private int readIndex = 0;
 
-    private long capacity;
-    private long size;
+    private int capacity;
+    private int size;
 
     public Servant(int capacity){
         this.capacity = capacity;
-        this.size = size;
+        this.size = 0;
+    }
+
+    public int nSpaceAvailable(){
+        return capacity-size;
+    }
+
+    public int nDataAvailable(){
+        return size;
+    }
+
+    public void put(Iterable<T> data, int n){
+        for(T elem : data){
+            buffer[writeIndex] = elem;
+            writeIndex = (writeIndex+1) % capacity;
+        }
+        size += n;
     }
 
     public ArrayList<T> get(int n){
-        var result = new ArrayList<>(n);
-        for(long i = readIndex; i < n; readIndex = (readIndex+1) % capacity){
-
+        var result = new ArrayList<T>(n);
+        for(int i = readIndex, taken = 0; taken < n; readIndex = (readIndex+1) % capacity, taken++){
+            result.add(buffer[i]);
         }
+        size -= n;
+        return result;
     }
 }
